097b87a7a343c5d1753ed77c0b4d3c4d4ba6c338
Eaglewzw <1460853569@qq.com>
2025-08-01 10:38:03 +0800

    first commit

---
+++ ros2_helloworld/include/ros2_helloworld/helloworld.h
@@ -0,0 +1,30 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+#include <chrono>
+#include <iostream>
+#include <rclcpp/node.hpp>
+#include <rclcpp/timer.hpp>
+
+class HelloWorld : public rclcpp::Node 
+{
+    public:
+        HelloWorld() : Node("helloworld") 
+        {
+
+            timer_ = this->create_wall_timer(
+                std::chrono::milliseconds(500),
+                std::bind(&HelloWorld::timer_callback, this)
+            );
+        }
+
+    private:
+        void timer_callback() 
+        {
+            RCLCPP_INFO(this->get_logger(), "say_something_node say: Hello world");
+        }
+
+        rclcpp::TimerBase::SharedPtr timer_;
+};
+
+#endif //HELLOWORLD_H
---
+++ ros2_helloworld/src/main.cpp
@@ -0,0 +1,15 @@
+#include "helloworld.h"
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char const *argv[])
+{
+    rclcpp::init(argc, argv);
+
+    auto say_hello_world_node = std::make_shared<HelloWorld>();
+
+    rclcpp::spin(say_hello_world_node);
+
+    rclcpp::shutdown();
+
+    return 0;
+}
